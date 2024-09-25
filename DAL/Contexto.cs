924ea54ee23ae1af65840f5994cea1a81e8a6bc6
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2024-09-25 19:19:35 -0400

    Carpetas,Paquetes,Ajustes

---
+++ DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DarvynLuisLavandier_AP1_P1.Models;
+namespace DarvynLuisLavandier_AP1_P1.DAL
+{
+    public class Contexto : DbContext
+    {
+        DbSet<Registros> Registros { get; set; }
+        public Contexto(DbContextOptions<Contexto>options) 
+            : base(options)
+        
+        { }
+    }
+}
---
+++ Models/Registros.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DarvynLuisLavandier_AP1_P1.Models
+{
+    public class Registros
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Nombres { get; set; }
+    }
+}
